87c4f33a57a9729f323f2f91302ad91431003e4c
DaniellaKatz <denissiniu@gmail.com>
2024-06-26 18:20:09 +0300

    Update homework3.py

--- homework3.py
+++ homework3.py
@@ -1,8 +1,9 @@
-name="Дана"
-print(name)
+name = "Dana"
+print('Name', name)
 age = 38
-print(age)
-age = 18
-print(age)
-is_student='True'
-print('is student',':',is_student)
+print('Age', age)
+new_age = age + int(1)
+print('New Age', new_age)
+is_student = 'True'
+is_cook = "False"
+print('Is student :', is_student)
